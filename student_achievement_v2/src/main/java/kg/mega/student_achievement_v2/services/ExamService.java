44322d899da77dc43e648198d46a7bf164f765ac
kulonbekov <kulonbekov@gmail.com>
2023-02-14 01:54:18 +0600

    Added exam date check for weekends and work time!

--- student_achievement_v2/src/main/java/kg/mega/student_achievement_v2/services/ExamService.java
+++ student_achievement_v2/src/main/java/kg/mega/student_achievement_v2/services/ExamService.java
@@ -3,10 +3,14 @@ package kg.mega.student_achievement_v2.services;
 import kg.mega.student_achievement_v2.models.dtos.ExamDto;
 import kg.mega.student_achievement_v2.models.dtos.SubjectDto;
 
+import java.util.Date;
+
 public interface ExamService {
 
     ExamDto save (ExamDto examDto);
     ExamDto findById(Long id);
+
+    Boolean checkDate(Date date, int duration);
 
 
 }
--- student_achievement_v2/src/main/java/kg/mega/student_achievement_v2/services/impl/ExamServiceImpl.java
+++ student_achievement_v2/src/main/java/kg/mega/student_achievement_v2/services/impl/ExamServiceImpl.java
@@ -8,6 +8,9 @@ import kg.mega.student_achievement_v2.services.ExamService;
 import kg.mega.student_achievement_v2.services.SubjectService;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+import java.util.Date;
+
 
 @Service
 public class ExamServiceImpl implements ExamService {
@@ -24,11 +27,16 @@ public class ExamServiceImpl implements ExamService {
 
     @Override
     public ExamDto save(ExamDto examDto) {
-        Exam exam = examMapper.examDtoToEntity(examDto);
-        exam = examRep.save(exam);
-        examDto.setId(exam.getId());
-        examDto.setSubjectDto(subjectService.findById(exam.getSubject().getId()));
-        return examDto;
+
+        if(checkDate(examDto.getExamDate(),examDto.getDuration())){
+            Exam exam = examMapper.examDtoToEntity(examDto);
+            exam = examRep.save(exam);
+            examDto.setId(exam.getId());
+            examDto.setSubjectDto(subjectService.findById(exam.getSubject().getId()));
+            return examDto;
+        }
+        System.out.println("Invalid date and time!");
+        return null;
     }
 
     @Override
@@ -36,5 +44,41 @@ public class ExamServiceImpl implements ExamService {
         Exam exam = examRep.findById(id).orElseThrow(()-> new RuntimeException("Экзамен не найден"));
         ExamDto examDto = examMapper.examToExamDto(exam);
         return examDto;
+    }
+
+    @Override
+    public Boolean checkDate(Date date, int duration) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        Calendar currentTime = Calendar.getInstance();
+        currentTime.setTime(new Date());
+
+        Calendar dateTime = Calendar.getInstance();
+        dateTime.setTime(date);
+        dateTime.add(Calendar.MINUTE,duration);
+
+        Calendar date1 = Calendar.getInstance();
+        date1.setTime(date);
+        date1.set(Calendar.HOUR_OF_DAY, 9);
+        date1.set(Calendar.MINUTE, 00);
+        date1.set(Calendar.SECOND, 00);
+
+        Calendar date2 = Calendar.getInstance();
+        date2.setTime(date);
+        date2.set(Calendar.HOUR_OF_DAY, 17);
+        date2.set(Calendar.MINUTE, 00);
+        date2.set(Calendar.SECOND, 00);
+
+        if(calendar.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY
+                || calendar.get(Calendar.DAY_OF_WEEK)==Calendar.SATURDAY
+                || calendar.get(Calendar.DAY_OF_YEAR)<(currentTime.get(Calendar.DAY_OF_YEAR))
+                || calendar.getTime().before(currentTime.getTime())
+                || calendar.getTime().before(date1.getTime())
+                || dateTime.getTime().after(date2.getTime())){
+                return false;
+
+        }
+        return true;
     }
 }
